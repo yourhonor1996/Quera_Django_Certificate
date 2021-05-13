69404f90d91e0aa90fca52ea8a90cadb43d19b80
Mohammd Alavi <yourhonor1996@gmail.com>
2021-05-13 20:16:40 +0430

    Finish Chapter 4

--- _All_Projects/4.6/web/urls.py
+++ _All_Projects/4.6/web/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+    path('sad/<str:name>/', make_sad),
+    path('happy/<str:name>/<int:times>/', make_happy)
 ]
--- _All_Projects/4.6/web/views.py
+++ _All_Projects/4.6/web/views.py
@@ -1,1 +1,8 @@
 from django.http import HttpResponse
+
+def make_sad(request, name):
+    return HttpResponse(f'Nobody likes you, {name}!')
+
+def make_happy(request, name, times):
+    msg = '<br>'.join([f'You are great, {name} :)' for i in range(times)])
+    return HttpResponse(msg)
