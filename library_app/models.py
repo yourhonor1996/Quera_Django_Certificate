a30d7450679d3a5a79be2735d54f72a066fbe0ab
Mohammd Alavi <yourhonor1996@gmail.com>
2021-07-10 02:46:53 +0430

    continue

--- library_app/models.py
+++ library_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -18,3 +18,21 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+class Profile(models.Model):
+    """Model definition for Profile."""
+
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    
+    phone_number = models.CharField(blank= True, null= True, max_length=50)
+    country = models.CharField(max_length=100)
+
+    class Meta:
+        """Meta definition for Profile."""
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """Unicode representation of Profile."""
+        pass
