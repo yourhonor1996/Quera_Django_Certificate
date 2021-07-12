68ba69b4fc9ba3ac353dc314a513cf36aac530ec
Mohammd Alavi <yourhonor1996@gmail.com>
2021-07-12 21:02:33 +0430

    Case Study Practice - Part 4

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render
+from accounts.models import User
+
 def about_us(request):
+    users = User.objects.all()
+    context = {'users':users}
     # View for about-us page
-    ...
+    return render(request, 'about_us.html', context= context)
