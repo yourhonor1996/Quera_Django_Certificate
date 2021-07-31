3f07aa3ae6264f4f8b4dd74b34d222b5094e6309
Mohammd Alavi <yourhonor1996@gmail.com>
2021-07-31 16:29:13 +0430

    Continue 15.11

--- _All_Projects/15.11/people/migrations/0001_initial.py
+++ _All_Projects/15.11/people/migrations/0001_initial.py
@@ -2,11 +2,8 @@ import json
 
 from django.conf import settings
 from django.db import migrations, models
-from django.db.migrations.operations.fields import AddField, AlterField
-from django.db.models import F, Value, CharField
-from django.db.models.functions import Concat, Replace
 
-from people.models import Person
+# from people.models import Person
 
 def insert_bad_data(apps, _):
     Person = apps.get_model('people', 'Person')
@@ -20,38 +17,26 @@ def insert_bad_data(apps, _):
 
 def ammend_the_data(apps, _):
     Person = apps.get_model('people', 'Person')
-    people = Person.objects.all()
-    
+    people = Person.objects.all().iterator()
+
     for person in people:
-        fullnamesplit = str(person.fullname).split(';')
+        fullnamesplit = person.fullname.split(';')
         first_name = fullnamesplit[0].split(':')[1]
         last_name = fullnamesplit[1].split(':')[1]
 
-        informationsplit = str(person.information).split(';')
+        informationsplit = person.information.split(';')
         info2 = {}
         for info in informationsplit:
             info3 = info.split(':')
             info2.update({info3[0] : info3[1]})
         
-        # person.update(first_name= first_name, last_name= last_name, id_code= info2.get('id_code'), born_in= info2.get('born_in'), birth_year= info2.get('birth_year'))
         person.first_name = first_name
         person.last_name = last_name
         person.id_code = info2.get('id_code')
         person.born_in = info2.get('born_in')
-        person.birth_year = info2.get('birth_year')
+        person.birth_year = int(info2.get('birth_year'))
+        person.save()
 
-
-
-    # fullname_split = (F('fullname')).split(';')
-    # first_name, last_name = 
-    
-    # Person.objects.update(first_name= )
-
-    
-
-    # Cake.objects.annotate(new_sauce_name=Concat(
-    #     F('sauce'), Value(' Sauce'), output_field=CharField())).update(sauce=F('new_sauce_name'))
-    
 
 
 class Migration(migrations.Migration):
@@ -67,6 +52,12 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('fullname', models.CharField(max_length=250, null=True)),
                 ('information', models.CharField(max_length=350, null=True)),
+                
+                ('first_name', models.CharField(max_length=30, null=True)),
+                ('last_name', models.CharField(max_length=50, null=True)),
+                ('id_code', models.CharField(max_length=10, null=True)),
+                ('born_in', models.CharField(max_length=30, null=True)),
+                ('birth_year', models.PositiveSmallIntegerField(null=True)),
             ],
         ),
         migrations.RunPython(
@@ -74,36 +65,36 @@ class Migration(migrations.Migration):
             reverse_code=migrations.RunPython.noop
         ),
 
-        migrations.AddField(
+        
+        migrations.RunPython(
+            code= ammend_the_data,
+            reverse_code=migrations.RunPython.noop),
+        
+        migrations.AlterField(
             model_name= 'Person', 
             name= 'first_name',
-            field= models.CharField(max_length= 30, null= True)),
-        migrations.AddField(
+            field= models.CharField(max_length= 30)),
+        migrations.AlterField(
             model_name= 'Person', 
             name= 'last_name',
-            field= models.CharField(max_length= 50, null= True)),
-        migrations.AddField(
+            field= models.CharField(max_length= 50)),
+        migrations.AlterField(
             model_name= 'Person', 
             name= 'id_code',
-            field= models.CharField(max_length= 10, null= True)),
-        migrations.AddField(
+            field= models.CharField(max_length= 10)),
+        migrations.AlterField(
             model_name= 'Person', 
             name= 'born_in',
-            field= models.CharField(max_length= 30, null= True)),
-        migrations.AddField(
+            field= models.CharField(max_length= 30)),
+        migrations.AlterField(
             model_name= 'Person', 
-            name= 'first_name',
-            field= models.PositiveSmallIntegerField(null= True)),
+            name= 'birth_year',
+            field= models.PositiveSmallIntegerField()),
         
-        
-        migrations.RunPython(code= ammend_the_data),
-        
-        # migrations.RemoveField(
-        #     model_name= 'Person',
-        #     name= 'fullname'),
-        # migrations.RemoveField(
-        #     model_name= 'Person',
-        #     name= 'information'),
-        
-        
+        migrations.RemoveField(
+            model_name= 'Person',
+            name= 'fullname'),
+        migrations.RemoveField(
+            model_name= 'Person',
+            name= 'information'),
     ]
--- _All_Projects/15.11/people/models.py
+++ _All_Projects/15.11/people/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Person(models.Model):
-    fullname = models.CharField(max_length=250, null=True)
-    information = models.CharField(max_length=350, null=True)
+    fullname = models.CharField(max_length=250, null= True)
+    information = models.CharField(max_length=350, null= True)
 
+    first_name = models.CharField(max_length=30, null= True)
+    last_name = models.CharField(max_length=50, null= True)
+    id_code = models.CharField(max_length=10, null= True)
+    born_in = models.CharField(max_length=30, null= True)
+    birth_year = models.PositiveSmallIntegerField(null= True)
