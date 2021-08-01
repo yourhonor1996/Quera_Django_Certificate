6341feeead26cc6059753f56df53d3e162a0d1f5
Mohammd Alavi <yourhonor1996@gmail.com>
2021-08-01 16:38:15 +0430

    edit 15.11

--- _All_Projects/15.11/people/migrations/0001_initial.py
+++ _All_Projects/15.11/people/migrations/0001_initial.py
@@ -15,29 +15,6 @@ def insert_bad_data(apps, _):
             information=person['fields']['information']
         )
 
-def ammend_the_data(apps, _):
-    Person = apps.get_model('people', 'Person')
-    people = Person.objects.all().iterator()
-
-    for person in people:
-        fullnamesplit = person.fullname.split(';')
-        first_name = fullnamesplit[0].split(':')[1]
-        last_name = fullnamesplit[1].split(':')[1]
-
-        informationsplit = person.information.split(';')
-        info2 = {}
-        for info in informationsplit:
-            info3 = info.split(':')
-            info2.update({info3[0] : info3[1]})
-        
-        person.first_name = first_name
-        person.last_name = last_name
-        person.id_code = info2.get('id_code')
-        person.born_in = info2.get('born_in')
-        person.birth_year = int(info2.get('birth_year'))
-        person.save()
-
-
 
 class Migration(migrations.Migration):
     initial = True
@@ -64,37 +41,4 @@ class Migration(migrations.Migration):
             code=insert_bad_data,
             reverse_code=migrations.RunPython.noop
         ),
-
-        
-        migrations.RunPython(
-            code= ammend_the_data,
-            reverse_code=migrations.RunPython.noop),
-        
-        migrations.AlterField(
-            model_name= 'Person', 
-            name= 'first_name',
-            field= models.CharField(max_length= 30)),
-        migrations.AlterField(
-            model_name= 'Person', 
-            name= 'last_name',
-            field= models.CharField(max_length= 50)),
-        migrations.AlterField(
-            model_name= 'Person', 
-            name= 'id_code',
-            field= models.CharField(max_length= 10)),
-        migrations.AlterField(
-            model_name= 'Person', 
-            name= 'born_in',
-            field= models.CharField(max_length= 30)),
-        migrations.AlterField(
-            model_name= 'Person', 
-            name= 'birth_year',
-            field= models.PositiveSmallIntegerField()),
-        
-        migrations.RemoveField(
-            model_name= 'Person',
-            name= 'fullname'),
-        migrations.RemoveField(
-            model_name= 'Person',
-            name= 'information'),
     ]
